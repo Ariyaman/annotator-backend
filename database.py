0435e5a050199aec03ee1c76eb530a6561122853
2SkeePeR2 <benten.ben14@gmail.com>
2022-07-26 19:41:47 +0530

    update db creds

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-DB_URL = "mysql+pymysql://doadmin:AVNS_J9Bp5lhLemduOjUZ8HI@annotator-db-do-user-12101645-0.b.db.ondigitalocean.com:25060/annotator"
+DB_URL = os.getenv("DB_URL")
 
 engine = create_engine(DB_URL)
 
