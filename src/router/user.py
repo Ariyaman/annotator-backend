ba99ba227e12b8ea70ffcb8e4d82a179beddae71
Ariyaman <ariyamangoswami@gmail.com>
2022-08-08 14:06:45 +0530

    added api endpoint to get article count by userid

--- src/router/user.py
+++ src/router/user.py
@@ -5,6 +5,7 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 from database import get_db
+from src.services.statement import get_statement_count_by_user_id_and_overall
 from src.services.user import create_user_service, get_user_by_email, get_user_by_id
 
 from src.models.user import LoginUserBody, UserCreate
@@ -65,3 +66,13 @@ def get_role_by_id(user_id: str, db: Session = Depends(get_db)):
             "role": selected_user.role,
             "name": selected_user.name
         }))
+
+@router.get("/get_marked_article_count_by_id/{user_id}")
+def get_article_count_by_user_id(user_id: str, db: Session = Depends(get_db)):
+    total_marked_article_count = get_statement_count_by_user_id_and_overall(db, user_id, True)
+
+    return JSONResponse(jsonable_encoder({
+        "count": total_marked_article_count
+    }), HTTPStatus.OK)
+
+
